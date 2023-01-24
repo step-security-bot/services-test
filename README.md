0222a6efcbdd38ff8e2927d0d4fa6692642ca3f0
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-01-24 18:33:29 +0100

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # services-test
+
+[![License][license-badge]][license]
+
+<!-- Markdown links -->
+
+[license]: https://opensource.org/licenses/MIT
+[license-badge]: https://img.shields.io/github/license/afuetterer/services-test
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,6 @@ from services_test import cli
 
 def test_hello_world() -> None:
     input_msg = "hello world"
-    result = cli.hello_world(input_msg)
-    output_msg = "HELLO WORLD"
-    assert result == output_msg
+    actual = cli.hello_world(input_msg)
+    expected = "HELLO WORLD"
+    assert actual == expected
