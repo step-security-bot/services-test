99befad7ac6cce3c11b2b60ec8ab1d8a424b152d
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-02-02 15:14:18 +0100

    test: add tests

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+from pytest_mock.plugin import MockerFixture
+
+import services_test
+
+
+def test_entrypoint_calls_app(mocker: MockerFixture) -> None:
+    mocker.patch("services_test.cli.app")
+    from services_test import __main__  # noqa: F401
+
+    services_test.cli.app.assert_called_once_with(prog_name="services-test")  # type: ignore
